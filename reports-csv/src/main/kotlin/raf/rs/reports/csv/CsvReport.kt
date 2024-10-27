418786ac3c2e26703b09d905286dba24af4d99be
Dimitrije Mijailovic <pinger.business@gmail.com>
2024-10-27 21:00:28 +0100

    feat: add csv report implementation

---
+++ reports-csv/src/main/kotlin/raf/rs/reports/csv/CsvReport.kt
@@ -0,0 +1,26 @@
+package raf.rs.reports.csv
+
+import raf.rs.reports.IReport
+import raf.rs.reports.ReportType
+import java.io.File
+
+class CsvReport : IReport {
+
+    override val getReportType: ReportType = ReportType.CSV
+
+    override fun generateReport(data: Map<String, List<String>>, destination: String, header: Boolean, title: String?, summary: String?) {
+        val columns = data.keys.toList()
+        val numRows = data.values.first().size
+
+        File(destination).printWriter().use { writer ->
+            if (header) {
+                writer.println(columns.joinToString(separator = ","))
+            }
+
+            for (i in 0 until numRows) {
+                val row = columns.map { column -> data[column]?.get(i) ?: "" }
+                writer.println(row.joinToString(separator = ","))
+            }
+        }
+    }
+}
