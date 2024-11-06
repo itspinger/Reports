e802fc319bd6e982a08f393f3dce3f2176baa267
avelickovic <aalleekkssa2@gmail.com>
2024-11-06 23:33:20 +0100

    Added almost all to main and found bug that format isn't nullable

--- reports-app/src/main/kotlin/Test.kt
+++ reports-app/src/main/kotlin/Test.kt
@@ -4,6 +4,8 @@ import raf.rs.reports.IReport
 import raf.rs.reports.ReportType
 import raf.rs.reports.calculations.SummaryCalculation
 import raf.rs.reports.calculations.SummaryCalculationType
+import raf.rs.reports.model.ElementProperties
+import raf.rs.reports.model.FormattingOptions
 import raf.rs.reports.model.Summary
 import java.io.File
 import java.io.FileReader
@@ -90,6 +92,7 @@ fun main() {
     calculation, printRowNumbers = true)
 
     */
+    var summaryCalculations = null as SummaryCalculation?
     var data: Map<String, List<String>> = emptyMap()
    var  implementation: IReport? = null;
     println("izaberite implementaciju za izvoz podataka : 1. TXT 2. EXCEL 3. PDF 4. CSV");
@@ -115,8 +118,11 @@ fun main() {
             println("Niste uneli validan broj");
         }
     }
+
+
     var i=0
     while(true){
+        var formatOptions= null as FormattingOptions?
         if(i!=0){
             println("Da li zelite da dodate jos jedan izvestaj 1. Da 2. Ne")
             if(readLine()=="2"){
@@ -213,76 +219,182 @@ fun main() {
         if(readLine()=="1"){
             isPrintRowNumbers = true;
         }
-        println("Da li zelite da dodate sazetak? 1. Da 2. Ne")
-        var summary = false;
-        var isCalcSummary=false
-        var calcSummary="";
-        var nonCalcSummary = "";
-        var summaryCalculations = SummaryCalculation("year", SummaryCalculationType.SUM, SummaryCalculation.Operator.GREATER_EQUAL, 3)
-        if(readLine()=="1"){
-            summary = true;
-        }
-        if(summary){
 
+            println("Da li zelite da dodate sazetak? 1. Da 2. Ne")
+            var summary = false;
+            var isCalcSummary = false
+            var calcSummary = "";
+            var nonCalcSummary = "";
+
+            if (readLine() == "1") {
+                summary = true;
+            }
+            var summaryLabel = "";
+            var summaryColumnName = "";
+            var summaryType = "";
+            //var summaryOperator = "";
+            var summaryOperator= null as SummaryCalculation.Operator?
+            if (summary) {
                 println("unesite naziv labele");
-                var summaryLabel = readLine();
-
-
+                summaryLabel = readLine().toString();
                 println("unesite tip sazetka 1. Izracunjiv 2. Slobadan");
-                if(readLine()=="1"){
+                if (readLine() == "1") {
                     isCalcSummary = true;
                     println("unesite ime kolone za koju zelite da izracunate sazetak");
-                    var summaryColumnName = readLine();
-                    println("unesite tip izracunavanja 1. SUM 2. AVG 3. COUNT");
-                    var summaryType = readLine();
-                    calcSummary= summaryType.toString()+" "+summaryColumnName.toString();
-                    println("unesite uslov ako zelite, ako ne zelite unesite enter");
-                    var summaryOperator = readLine();
-                    calcSummary+= " "+summaryOperator.toString();
+                    summaryColumnName = readLine().toString()
+                    println("unesite tip izracunavanja SUM  AVG  COUNT");
+                    summaryType = readLine().toString()
+                    calcSummary = summaryType.toString() + " " + summaryColumnName.toString();
+                    println("unesite operator za uslov > < >= <= != =, ako ne zelite unesite enter")
+
+                    when(readLine()){
+                        ">" -> summaryOperator= SummaryCalculation.Operator.GREATER
+                        "<" -> summaryOperator= SummaryCalculation.Operator.LESSER
+                        ">=" -> summaryOperator= SummaryCalculation.Operator.GREATER_EQUAL
+                        "<=" -> summaryOperator= SummaryCalculation.Operator.LESS_EQUAL
+                        "!=" -> summaryOperator= SummaryCalculation.Operator.NOT_EQUAL
+                    }
+                    println("summaryOperator"+summaryOperator)
+
+                    var value = 0;
+                    println("unesite vrednost za uslov")
+                    if(summaryOperator!=null) {
+                        value = readLine()?.toInt() as Int
+                        summaryCalculations = SummaryCalculation(summaryColumnName, SummaryCalculationType.valueOf(summaryType),summaryOperator, value)
+                    }
+                    else
+                        summaryCalculations = SummaryCalculation(summaryColumnName, SummaryCalculationType.valueOf(summaryType))
+
+                    println("value"+value)
+
+                    calcSummary += " " + summaryOperator.toString();
+                    calcSummary += " " + value.toString();
 
 
-                }
-                else{
+                } else {
                     println("unesite sazetak")
                     nonCalcSummary = readLine().toString()
                 }
-                if(isCalcSummary)
-                    exporterServices[implementation!!.getReportType]?.generateReport( //ToDo treba da se doda summary
+
+
+
+            }
+            if(implementation!!.getReportType == ReportType.EXCEL || implementation!!.getReportType == ReportType.PDF) {
+
+
+                println("Da li zelite da dodate formatiranje? 1. Da 2. Ne")
+                if (readLine() == "1") {
+                    formatOptions = FormattingOptions()
+                    var style = "";
+                    var color = "";
+                    while (true) {
+                        println("Birajte sta zelite da formatirate 1. Naslov 2. Header 3. Red 4. Kolonu 5.Sažetak ako ne zelite da dodate vise formatiranja pretisnite enter")
+                        var formatOption = readLine();
+                        if (formatOption == "") {
+                            break;
+                        }
+                        println("unesite boju crvena plava zelena");
+                        color = readLine().toString();
+                        println("unesite stil  BOLD ITALIC  UNDERLINE");
+                        style = readLine().toString();
+                        if (color == "crvena") {
+                            color = "#f72105";
+                        } else if (color == "plava") {
+                            color = "#22d3d6";
+                        } else if (color == "zelena") {
+                            color = "#22d6a6";
+                        }
+
+
+                        when (formatOption) {
+                            "1" -> {
+                                formatOptions.titleFormat =
+                                    ElementProperties.of(color, ElementProperties.TextStyle.valueOf(style))
+                            }
+
+                            "2" -> {
+                                formatOptions.headerFormat =
+                                    ElementProperties.of(color, ElementProperties.TextStyle.valueOf(style))
+                            }
+
+                            "3" -> {
+                                println("Unesite redni broj reda koji zelite da formatirate")
+                                var rowNumber = readLine().toString().toInt();
+                                formatOptions.rowFormat[rowNumber] =
+                                    ElementProperties.of(color, ElementProperties.TextStyle.valueOf(style))
+                            }
+
+                            "4" -> {
+                                println("Unesite ime kolone koju zelite da formatirate")
+                                var columnName = readLine().toString();
+                                formatOptions.columnFormat[columnName] =
+                                    ElementProperties.of(color, ElementProperties.TextStyle.valueOf(style))
+                            }
+
+                            "5" -> {
+                                formatOptions.summaryFormat[summaryLabel] =
+                                    ElementProperties.of(color, ElementProperties.TextStyle.valueOf(style))
+                            }
+                        }
+                        println("Da li zelite da promenite debljinu linje? 1. Da 2. Ne")
+                        if (readLine() == "1") {
+                            println("Unesite debljinu linije 1. NONE 2. THIN 3. MEDIUM")
+                            var borderStyle = readLine().toString();
+                            when (borderStyle) {
+                                "1" -> {
+                                    formatOptions.borderStyle = FormattingOptions.BorderStyle.NONE
+                                }
+
+                                "2" -> {
+                                    formatOptions.borderStyle = FormattingOptions.BorderStyle.THIN
+                                }
+
+                                "3" -> {
+                                    formatOptions.borderStyle = FormattingOptions.BorderStyle.MEDIUM
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if(summary){
+                if (isCalcSummary)
+                    exporterServices[implementation!!.getReportType]?.generateReport(
+                        data,
+                        destination = destinationfile,
+                        header = header,
+                        title = title,
+                        format = formatOptions!!,
+                        summary = Summary(mapOf(summaryLabel.toString() to summaryCalculations as Any)),
+
+
+                    )
+                else {
+                    exporterServices[implementation!!.getReportType]?.generateReport(
+                        data,
+                        destination = destinationfile,
+                        header = header,
+                        title = title,
+                        mapOf<String, String>(Pair(summaryLabel.toString(), nonCalcSummary.toString())),
+                        format = formatOptions!!,
+                        printRowNumbers = isPrintRowNumbers
+
+                    )
+                }
+            }
+            else{
+                exporterServices[implementation!!.getReportType]?.generateReport(
                     data,
                     destination=destinationfile,
                     header = header,
                     title = title,
-                        //mapOf<String, String>(Pair(summaryLabel.toString(),calcSummary.toString()))
-                        summary=Summary(mapOf("Avg Years" to summaryCalculations))
-                    )
-                else{
-                    exporterServices[implementation!!.getReportType]?.generateReport(
-                        data,
-                        destination=destinationfile,
-                        header = header,
-                        title = title,
-                        mapOf<String, String>(Pair(summaryLabel.toString(),nonCalcSummary.toString())) ,
-                        printRowNumbers = isPrintRowNumbers
+                    summary = null as Summary?,
+                    printRowNumbers = isPrintRowNumbers,
+                    format = formatOptions!!
+                )
 
+            }
 
-                    )
-                }
-
-
-
-        }
-        else{
-
-            exporterServices[implementation!!.getReportType]?.generateReport(
-                data,
-                destination=destinationfile,
-                header = header,
-                title = title,
-                summary = null as Summary?,
-                printRowNumbers = isPrintRowNumbers,
-            )
-
-        }
         i++
     }
 }  
